6eacbf279a3a0e721390c650d88f7339687aa8e1
bella77-69 <mrspasceri@hotmail.com>
2025-08-31 20:19:32 -0700

    update cors

--- server/server.js
+++ server/server.js
@@ -12,12 +12,24 @@ const apiRoutes = require('./routes/api');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors(
-    {
-        origin: 'https://acknowledgingourlands-server.onrender.com',
-        credentials: true
+
+const allowedOrigins = [
+  'https://acknowledging-our-lands.vercel.app', 
+  'https://acknowledgingourlands-server.onrender.com'
+];
+
+app.use(cors({
+    origin: function(origin, callback){
+    // allow requests with no origin (like Postman)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
     }
-));
+    return callback(null, true);
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(helmet());
